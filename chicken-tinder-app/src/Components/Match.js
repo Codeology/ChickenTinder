f92799a32748c880e027b20cb8b514a163f6b426
Jennifer Buja <jlbuja@berkeley.edu>
2020-08-02 17:40:03 -0700

    changed components to classes and changed functionality of index.js to only render front page

--- chicken-tinder-app/src/Components/Match.js
+++ chicken-tinder-app/src/Components/Match.js
@@ -1,25 +1,32 @@
 import React from "react";
+import { render } from "react-dom";
 
-function Match(props) {
-    return(
-      <div id="match">
-        <div className='Match'>
-          <h1> It's a match!</h1> 
-        </div>
-        <div className="RestaurantInfo-Name">
-            <h1>{props.name}</h1>
-         </div>
-        <img
-            className="img"
-            src={props.imgURL}
-            alt={props.name}
-          />
-        <div className="RestaurantInfo-Loc">
-            <h2> Address: {props.location} </h2>
-        </div>
-        <p> ♡ </p> 
-      </div>
-    );
-  }
+class Match extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return(
+            <div id="match">
+                <div className='Match'>
+                <h1> It's a match!</h1> 
+                </div>
+                <div className="RestaurantInfo-Name">
+                    <h1>{this.props.name}</h1>
+                </div>
+                <img
+                    className="img"
+                    src={this.props.imgURL}
+                    alt={this.props.name}
+                />
+                <div className="RestaurantInfo-Loc">
+                    <h2> Address: {this.props.location} </h2>
+                </div>
+                <p> ♡ </p> 
+            </div>
+        );
+    }
+}
 
 export default Match;
--- chicken-tinder-app/src/Components/Profile.js
+++ chicken-tinder-app/src/Components/Profile.js
@@ -1,34 +1,42 @@
 import React from "react";
+import { render } from "react-dom";
 
-function Profile(props) {
-    return (
-      <div className="Profile">
-        <div className="RestaurantInfo">
-          <img
-            className="img"
-            src={props.imgURL}
-            alt={props.name}
-          />
-          <div className="RestaurantInfo-Name">
-            {props.name}
-          </div>
-          <div className="RestaurantInfo-Rating">
-            {props.rating}
-          </div>
-          <div className="RestaurantInfo-Loc">
-            {props.location}
-          </div>
-          <div>
-            <form>
-            <input type="submit" name='swipe' value="Let's go!" onclick="" method="POST" action="/"/>
-            <input type="submit" name='swipe' value="Not Interested." onclick="" method="POST" action="/"/>
-            </form>
-          </div>
-        </div>
-      </div>
-    );
-  }
+class Profile extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            match: 0
+        }
+    }
 
-var match = 0;
+    render() {
+        return (
+            <div className="Profile">
+                <div className="RestaurantInfo">
+                <img
+                    className="img"
+                    src={this.props.imgURL}
+                    alt={this.props.name}
+                />
+                <div className="RestaurantInfo-Name">
+                    {this.props.name}
+                </div>
+                <div className="RestaurantInfo-Rating">
+                    {this.props.rating}
+                </div>
+                <div className="RestaurantInfo-Loc">
+                    {this.props.location}
+                </div>
+                <div>
+                    <form>
+                    <input type="submit" name='swipe' value="Let's go!" onclick="" method="POST" action="/"/>
+                    <input type="submit" name='swipe' value="Not Interested." onclick="" method="POST" action="/"/>
+                    </form>
+                </div>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default Profile;
